b1d580dcd0caf4d3a54512d1483dd5bda840341e
BoRamSon <onbrson1@gmail.com>
2024-12-04 02:46:52 +0900

    feat(contorller): '[출력] 현재 재고 출력' 기능 구현 완료

--- src/controller/MainController.js
+++ src/controller/MainController.js
@@ -1,17 +1,16 @@
-import { MissionUtils } from '@woowacourse/mission-utils';
 import Greeting from '../model/Greeting.js';
+import productsManager from '../model/productsManager.js';
+import InputView from '../view/InputView.js';
 
 class MainController {
   constructor() {
-    //
+    this.inputView = new InputView();
+    this.productManager = new productsManager();
   }
 
   async openStore() {
     new Greeting();
-  }
-
-  async re() {
-    //
+    this.productManager.printProducts();
   }
 }
 
