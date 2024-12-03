c1d1152280fa14b36cb9a2af62c4dfa6c0cde60d
BoRamSon <onbrson1@gmail.com>
2024-12-04 03:41:19 +0900

    feat(model): [입력] 구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])

--- src/model/ProductsManager.js
+++ src/model/ProductsManager.js
@@ -1,3 +1,5 @@
+import { ERROR_MESSAGES } from '../constants/errorMessages.js';
+import InputView from '../view/InputView.js';
 import OutputView from '../view/OutputView.js';
 import products from './products.js';
 
@@ -7,6 +9,7 @@ class ProductsManager {
 
   constructor() {
     this.existingProducts = products;
+    this.inputView = new InputView();
   }
 
   printProducts() {
@@ -23,6 +26,17 @@ class ProductsManager {
       if (quantity === '0개') quantity = '';
       OutputView.printProducts(array.name, array.price, quantity, promotion);
     });
+  }
+
+  async inputItemsAndQuantity() {
+    for (let i = 0; i <= 5; i++) {
+      try {
+        return await this.inputView.readItemsAndQuantity();
+      } catch (error) {
+        OutputView.printError(error);
+        if (i === 5) throw new Error(ERROR_MESSAGES.enteredMoreFiveTimes);
+      }
+    }
   }
 
   productsUpdate() {
